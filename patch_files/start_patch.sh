109c5d5192e2e855d9e84c71f266b15af2eb0750
sengkyaut <sengkyaut@hotmail.com>
2021-12-15 13:36:07 +0000

    postpone tess_util.sh patch

--- patch_files/start_patch.sh
+++ patch_files/start_patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 patch_dir=$(dirname $0)
-patch /usr/share/tesseract-ocr/tesstrain_utils.sh -i $patch_dir/tesstrain_utils.patch
+#patch /usr/share/tesseract-ocr/tesstrain_utils.sh -i $patch_dir/tesstrain_utils.patch
 patch $OCR_WORKSPACE/tesseract/src/training/unicharset/validate_myanmar.cpp -i $patch_dir/validate_myanmar.patch
